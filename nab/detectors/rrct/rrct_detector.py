9d9ae584055094951294605cf1f378d70ca15f57
Or Groman <or.groman@kryonsystems.com>
2023-03-10 16:19:48 +0200

    WIP
    WIP

--- nab/detectors/rrct/rrct_detector.py
+++ nab/detectors/rrct/rrct_detector.py
@@ -10,14 +10,18 @@ def sigmoid(x):
     return result
 
 
+def sigmoid_array(x):
+    return 1 / (1 + np.exp(-x))
+
+
 class RrctDetector(AnomalyDetector):
     def __init__(self, *args, **kwargs):
         super(RrctDetector, self).__init__(*args, **kwargs)
-        self._num_trees = {}.get('num_trees', 40)
+        self._num_trees = {}.get('num_trees', 100)
 
         # Use reservoir sampling to drop or insert points
         self._tree_size = {}.get('tree_size', 256)
-        self._shingle_size = {}.get('shingle_size', 6)
+        self._shingle_size = {}.get('shingle_size', 4)
         self._codisp_threshold = 80
         self._codisp_list = []
         # self._points_array = np.arange(self._tree_size)
@@ -27,7 +31,7 @@ class RrctDetector(AnomalyDetector):
         # Create a forest of empty trees
         self._forest = []
         for _ in range(self._num_trees):
-            tree = rrcf.RCTree()
+            tree = rrcf.RCTree(precision=2)
             self._forest.append(tree)
 
     def handleRecord(self, inputData):
@@ -41,33 +45,73 @@ class RrctDetector(AnomalyDetector):
             return (0,)
 
         tree_count = 0
+        min_codisp = 1e9
+        codisp_list = []
+
         for tree in self._forest:
             # If tree is above permitted size...
-            k = random.randint(0, 1)
-            if k > 0:
-                tree_count+=1
-                point_idx = len(tree.leaves)
-                if len(tree.leaves) >= self._tree_size:
-                    # Insert the new point into the tree
-                    point_idx = int(random.random() * self._tree_size)
-                    tree.forget_point(index=point_idx)
-                tree.insert_point(self._shingle_points, index=point_idx)
-                avg_codisp += tree.codisp(point_idx) / tree_count
+            tree_count += 1
+            self._point_idx = len(tree.leaves)
+            if len(tree.leaves) >= self._tree_size:
+                # Insert the new point into the tree
+                self._point_idx = int(random.random() * self._tree_size)
+                tree.forget_point(index=self._point_idx)
+            tree.insert_point(self._shingle_points, index=self._point_idx)
+            point_codisp = tree.codisp(self._point_idx)
+            avg_codisp += point_codisp / tree_count
+            min_codisp = min(min_codisp, point_codisp)
+            codisp_list.append(point_codisp)
 
-        # self._point_idx+=1
-        # self._point_idx=self._point_idx%self._tree_size
+        median_codisp = np.median(codisp_list)
 
-        # self._codisp_list.append(avg_codisp)
-        # if len(self._codisp_list) > self._shingle_size:
-        #     self._codisp_list.pop(0)
+        codisp_std = np.std(codisp_list)
+        codisp_mean = np.mean(codisp_list)
+
+        if codisp_std != 0:
+            zscore_codisp = (np.array(codisp_list)-codisp_mean)/codisp_std
+            sig_array = sigmoid_array(zscore_codisp)
+        else:
+            sig_array = np.zeros_like(codisp_list)
+
+        #print(f'max={sig_array.max()}, median={np.median(sig_array)}, mean={np.mean(sig_array)}, min={sig_array.min()}')
+
+        # self._point_idx += 1
+        # self._point_idx = self._point_idx % self._tree_size
+
+        self._codisp_list.append(avg_codisp)
+        if len(self._codisp_list) > 100:
+            self._codisp_list.pop(0)
+
         #
-        # qt = np.quantile(self._codisp_list, 0.999)
+        qt = np.median(self._codisp_list) + 2*np.std(self._codisp_list)
+        # if avg_codisp > qt:
+        #     print(f'qt = {qt}, avg = {avg_codisp}, median={median_codisp}, min={min_codisp}')
+
+        #qt = np.quantile(self._codisp_list, 0.999)
+        # if avg_codisp < qt:
+        #     avg_codisp = 0
+
         # self._codisp_threshold = qt
 
         # result = np.quantile(self._codisp_list, 0.99)
         #
-        result_num = 1-1/(1+np.log(1+avg_codisp))
-        #result = result_num if avg_codisp > qt else 0
-        #result = avg_codisp > self._codisp_threshold
+        result_num = 1 - 1 / (1 + np.log(1 + median_codisp))
+        #result_num = 1-1/(1+median_codisp)
+        mean_sig_array = np.mean(sig_array)
+        median_sig_array = np.median(sig_array)
+        max_sig_array = np.max(sig_array)
 
-        return (result_num,)
+        if max_sig_array > 0.9 and mean_sig_array > 0.5:
+            print(f'max={sig_array.max()}, median={median_sig_array}, mean={np.mean(sig_array)}, min={sig_array.min()}')
+
+        # if result_num > 0.8:
+        #     print(f'Anomaly! qt = {qt}, avg = {avg_codisp}, median={median_codisp}, min={min_codisp}')
+
+        # result = result_num if min_codisp > qt else 0
+        # if result > 0:
+        #     print(f'Anomaly! - {result}, qt = {qt}, avg = {avg_codisp}, median={median_codisp}, min={min_codisp}')
+
+        # result = avg_codisp > self._codisp_threshold
+
+        # print(self._codisp_list)
+        return (max_sig_array,)
